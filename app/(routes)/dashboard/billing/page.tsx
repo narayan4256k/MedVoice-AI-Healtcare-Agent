39994d0aa03588f9fff715331b2233d1945fde23
Narayan Malvankar <88489426+narayan4256k@users.noreply.github.com>
2025-12-03 12:04:48 +0530

    Delete app directory

--- app/(routes)/dashboard/billing/page.tsx
+++
@@ -1,9 +0,0 @@
-import { PricingTable } from '@clerk/nextjs'
-
-export default function PricingPage() {
-  return (
-    <div className='max-w-5xl mx-auto px-8 py-10 bg-slate-950'>
-      <PricingTable />
-    </div>
-  )
-}
